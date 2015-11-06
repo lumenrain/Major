9519097670b3813ae78c1f05ceb18325c7a249f3
lumenrain <lumenrain@gmail.com>
2015-11-06 01:12:58 +0100

    getting data from local mongoDB

--- server.js
+++ server.js
@@ -25,20 +25,30 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb://localhost/multivision');
+mongoose.connect('mongodb://localhost/Major');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
     console.log('Major db has started');
 });
 
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
 
-app.get('*', function(req, res){
-    res.render('index');
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
 });
 
 var port  = 1337;
